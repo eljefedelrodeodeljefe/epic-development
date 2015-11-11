326d595ce8d624f194e991d3f9b00966a666679c
eljefedelrodeodeljefe <robert.lindstaedt@gmail.com>
2015-11-11 22:14:01 +0100

    reducing server

--- server/app.js
+++ server/app.js
@@ -3,19 +3,19 @@ var koa = require('koa')
 
 var serve = require('koa-static')
 var logger = require('koa-logger')
-var router = require('koa-router')()
+// var router = require('koa-router')()
 
 
 var app = koa()
 
 app.use(serve('./build'))
 app.use(logger())
-app.use(router.routes())
-app.use(router.allowedMethods());
-
-router
-  .get('/', function *(next) {
-    console.log('Files are being served');
-  })
+// app.use(router.routes())
+// app.use(router.allowedMethods());
+//
+// router
+//   .get('/', function *(next) {
+//     console.log('Files are being served');
+//   })
 
 app.listen(3155)
