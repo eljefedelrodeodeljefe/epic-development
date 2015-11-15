26b69357cdde78ffd6379bde98bc0d114e1a2c76
eljefedelrodeodeljefe <robert.lindstaedt@gmail.com>
2015-11-15 21:12:55 +0100

    Revert "Merge remote-tracking branch 'origin/feature/dev'"
    This reverts commit f322bf298a0c6a1ab96821cd820fb8e08dab8fe4, reversing
    changes made to 0542e44936c3429acefcf60b57438cd4c971683f.
    
    # Conflicts:
    #	build/bundle.css

--- client/components/pages/home/elements/5_box_1/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  template: require('./template.html'),
-  components: {
-
-  }
-}
--- client/components/pages/home/index.js
+++ client/components/pages/home/index.js
@@ -1,9 +1,8 @@
 import Counter from '../../counter/index.js'
 import Splash from './elements/1_splash/index.js'
-import Leistungen from './elements/2_leistungen/index.js'
+import Subhead1 from './elements/2_subhead_1/index.js'
 import Subhead2 from './elements/3_subhead_2/index.js'
-import Kontakt from './elements/4_kontakt/index.js'
-import Box1 from './elements/5_box_1/index.js'
+import Box1 from './elements/4_box_1/index.js'
 
 import FooterNav from '../../layouts/footer-nav/index.js'
 
@@ -11,9 +10,8 @@ module.exports = {
   template: require('./template.html'),
   components: {
     Splash,
-    Leistungen,
+    Subhead1,
     Subhead2,
-    Kontakt,
     Box1,
     FooterNav
     // Counter
