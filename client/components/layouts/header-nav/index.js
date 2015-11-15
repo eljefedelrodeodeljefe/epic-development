764e025249571d3672793613cd5749c65e485e7a
eljefedelrodeodeljefe <robert.lindstaedt@gmail.com>
2015-11-16 00:13:22 +0100

    closes: jump to hack

--- client/components/layouts/header-nav/index.js
+++ client/components/layouts/header-nav/index.js
@@ -4,5 +4,11 @@ module.exports = {
     return
       msg: 'Hello from Component Home!'
   },
-  template: require('./template.html')
+  template: require('./template.html'),
+  methods: {
+  jump: function (h) {
+    var pos = document.getElementById(h).offsetTop;
+    window.scrollTo(0, pos);
+  }
+}
 };
