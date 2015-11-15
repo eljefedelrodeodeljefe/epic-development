9c91337972f5d436efff1c3a45ecfc333d32c2c7
eljefedelrodeodeljefe <robert.lindstaedt@gmail.com>
2015-11-15 20:29:11 +0100

    closes fixed ratio header; adds leistungen

--- client/components/pages/home/index.js
+++ client/components/pages/home/index.js
@@ -1,6 +1,6 @@
 import Counter from '../../counter/index.js'
 import Splash from './elements/1_splash/index.js'
-import Subhead1 from './elements/2_subhead_1/index.js'
+import Leistungen from './elements/2_leistungen/index.js'
 import Subhead2 from './elements/3_subhead_2/index.js'
 import Box1 from './elements/4_box_1/index.js'
 
@@ -10,7 +10,7 @@ module.exports = {
   template: require('./template.html'),
   components: {
     Splash,
-    Subhead1,
+    Leistungen,
     Subhead2,
     Box1,
     FooterNav
