dc7760558f91e29e11f823cc8b902459bf44656b
eljefedelrodeodeljefe <robert.lindstaedt@gmail.com>
2015-11-20 22:24:27 +0100

    removed vodka pic from slideshow

--- client/components/pages/home/elements/1_splash/index.js
+++ client/components/pages/home/elements/1_splash/index.js
@@ -1,7 +1,7 @@
 
 // REVIEW: hacky since last item of array for it scrolling reverse
 let links = [
-  'http://res.cloudinary.com/jefe-io/image/upload/c_scale,w_1314/v1447617124/1548168_331138637048654_2974130967997751976_o_abegam.jpg',
+  // 'http://res.cloudinary.com/jefe-io/image/upload/c_scale,w_1314/v1447617124/1548168_331138637048654_2974130967997751976_o_abegam.jpg',
   'http://res.cloudinary.com/jefe-io/image/upload/c_scale,w_1322/v1447617129/10273303_295558303940021_3122393801162874264_o-1_kbap6k.jpg',
   'http://res.cloudinary.com/jefe-io/image/upload/c_scale,w_1322/v1447617127/1911020_331141580381693_523657765020535647_o_tc7yxu.jpg',
   'http://res.cloudinary.com/jefe-io/image/upload/v1447757102/splash-home_zhsrro_pdh7ue.jpg'
