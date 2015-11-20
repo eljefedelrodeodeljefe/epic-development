1da1ea38630ecec1a5454e00d7c6294547b9a5d1
eljefedelrodeodeljefe <robert.lindstaedt@gmail.com>
2015-11-20 22:55:25 +0100

    Changed referenzen to real data structure;
    TODO: implement no-op for anchors w/o link

--- client/components/pages/home/elements/3_referenzen/index.js
+++ client/components/pages/home/elements/3_referenzen/index.js
@@ -3,15 +3,46 @@ module.exports = {
   data: function() {
     return {
       links: [
-        'http://res.cloudinary.com/jefe-io/image/upload/v1447630443/Chakuza-Curese-FB-Event-A_ibo6wr.jpg',
-        'http://res.cloudinary.com/jefe-io/image/upload/v1447630443/Traumfrauen-WP2_v4xxol.jpg',
-        'http://res.cloudinary.com/jefe-io/image/upload/v1447630443/10426765_4292954777773_7009074514962987474_n_brbmpt.jpg',
-        'http://res.cloudinary.com/jefe-io/image/upload/v1447630442/10404165_774527639285100_6229034639274008671_n_x48tp4.jpg',
-        'http://res.cloudinary.com/jefe-io/image/upload/v1447630442/10259951_10151979979676783_635858087890241140_n_icfloa.jpg',
-        'http://res.cloudinary.com/jefe-io/image/upload/v1447630442/995624_423835524445631_5365721727983521164_n_pawkqb.jpg',
-        'http://res.cloudinary.com/jefe-io/image/upload/v1447630442/156057_345829545579563_5967578341148828480_n_z9e71u.jpg',
-        'http://res.cloudinary.com/jefe-io/image/upload/v1447630442/1653459_374818349365867_3036915616163435448_n_fpwyl4.jpg',
-
+        {
+          name: 'Chakuza & Curse',
+          external_link: '#',
+          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447630443/Chakuza-Curese-FB-Event-A_ibo6wr.jpg'
+        },
+        {
+          name: '',
+          external_link: '#',
+          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447630443/Traumfrauen-WP2_v4xxol.jpg',
+        },
+        {
+          name: 'THE BLUEPRINT - FÉTE DE L´ORIGINALS',
+          external_link: 'https://www.facebook.com/media/set/?set=a.331138503715334.1073741835.293954250767093&type=3',
+          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447630443/10426765_4292954777773_7009074514962987474_n_brbmpt.jpg'
+        },
+        {
+          name: '',
+          external_link: '#',
+          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447630442/10404165_774527639285100_6229034639274008671_n_x48tp4.jpg'
+        },
+        {
+          name: 'ADBOYS4LIFE - MR.BOBFILMS X DOJO/FUCKINGYEAH X EPIC!DEVELOPMENT',
+          external_link: 'https://www.facebook.com/media/set/?set=a.306535136175671.1073741832.293954250767093&type=3',
+          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447630442/10259951_10151979979676783_635858087890241140_n_icfloa.jpg'
+        },
+        {
+          name: '',
+          external_link: '#',
+          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447630442/995624_423835524445631_5365721727983521164_n_pawkqb.jpg'
+        },
+        {
+          name: '',
+          external_link: '#',
+          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447630442/156057_345829545579563_5967578341148828480_n_z9e71u.jpg'
+        },
+        {
+          name: '',
+          external_link: '#',
+          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447630442/1653459_374818349365867_3036915616163435448_n_fpwyl4.jpg'
+        }
       ]
     }
   },
