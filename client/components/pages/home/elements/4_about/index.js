72b53460f2f4f0a15d61440cbd256826529f116b
eljefedelrodeodeljefe <robert.lindstaedt@gmail.com>
2015-11-20 23:36:54 +0100

    Added: new partners
    adapted resources
    href to new tab

--- client/components/pages/home/elements/4_about/index.js
+++ client/components/pages/home/elements/4_about/index.js
@@ -3,16 +3,81 @@ module.exports = {
   data: function() {
     return {
       partners: [
-        'http://res.cloudinary.com/jefe-io/image/upload/v1447672004/epic_partner_logo_p7s1_kzkgjg.png',
-        'http://res.cloudinary.com/jefe-io/image/upload/v1447672003/epic_partner_logo_flaconi_aclys4.png',
-        'http://res.cloudinary.com/jefe-io/image/upload/v1447672004/epic_partner_logo_deezer_wvc1wm.png',
-        'http://res.cloudinary.com/jefe-io/image/upload/v1447672004/epic_partner_logo_berliner_pilsener_tbxwzp.png',
-        'http://res.cloudinary.com/jefe-io/image/upload/v1447672003/epic_partner_logo_absolut_uihq8f.png',
-        'http://res.cloudinary.com/jefe-io/image/upload/v1447672002/epic_partner_logo_flaconi_neo_jbsdyo.png',
-        'http://res.cloudinary.com/jefe-io/image/upload/v1447672004/epic_partner_logo_pernod_cwt09c.png',
-        'http://res.cloudinary.com/jefe-io/image/upload/v1447672003/epic_partner_logo_fritz_k6uegl.png',
-        'http://res.cloudinary.com/jefe-io/image/upload/v1447672006/epic_partner_logo_champagner_c6mqnh.png',
-        'http://res.cloudinary.com/jefe-io/image/upload/v1447672005/epic_partner_logo_starwatch_ixudb9.png'
+        {
+          name: 'ProSiebenSat1',
+          external_link: 'http://prosiebensat1.com/de/startseite',
+          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447672004/epic_partner_logo_p7s1_kzkgjg.png'
+        },
+        {
+          name: 'Starwatch',
+          external_link: 'https://www.starwatch.de',
+          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447672005/epic_partner_logo_starwatch_ixudb9.png'
+        },
+        {
+          name: 'Flaconi',
+          external_link: 'http://www.flaconi.de',
+          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447672003/epic_partner_logo_flaconi_aclys4.png'
+        },
+        {
+          name: 'Deezer',
+          external_link: 'http://www.deezer.com',
+          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447672004/epic_partner_logo_deezer_wvc1wm.png'
+        },
+        {
+          name: 'Berliner Pilsener',
+          external_link: 'http://www.berliner-pilsner.de',
+          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447672004/epic_partner_logo_berliner_pilsener_tbxwzp.png'
+        },
+        {
+          name: 'Absolut',
+          external_link: 'http://www.absolut.com/de',
+          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447672003/epic_partner_logo_absolut_uihq8f.png'
+        },
+        {
+          name: 'FLACONI NEO',
+          external_link: 'http://www.flaconi-neo.de',
+          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447672002/epic_partner_logo_flaconi_neo_jbsdyo.png'
+        },
+        {
+          name: 'Pernod Ricard',
+          external_link: 'http://www.pernodricard.de',
+          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447672004/epic_partner_logo_pernod_cwt09c.png'
+        },
+        {
+          name: 'fritz-kola',
+          external_link: 'http://www.fritz-kola.de',
+          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1448057634/epic_partner_logo_fritz_jxoige.png'
+        },
+        {
+          name: 'Nicolas Feuillatte',
+          external_link: 'http://www.nicolas-feuillatte.com',
+          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447672006/epic_partner_logo_champagner_c6mqnh.png'
+        },
+        {
+          name: 'Pot of Gold:',
+          external_link: 'http://www.potofgold-skincare.com',
+          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1448057582/epic_partner_logo_pot-of-gold_zg1yvb.png'
+        },
+        {
+          name: 'MESANIC MUSIC',
+          external_link: 'http://www.mesanicmusic.com/',
+          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1448058626/epic_partner_logo_mesanic_ngdn3s.png'
+        },
+        {
+          name: 'UNBEEINDRUCKT',
+          external_link: 'http://www.unbeeindruckt.com ',
+          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1448058626/epic_partner_logo_unbeeindruckt_cmojxl.png'
+        },
+        {
+          name: 'EEEShop',
+          external_link: 'http://eeeshop.de',
+          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1448058626/epic_partner_logo_eee_wvhl2u.png'
+        },
+        {
+          name: 'Babylon',
+          external_link: '',
+          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1448058626/epic_partner_logo_babylon_edyxls.png'
+        }
       ]
     }
   },
