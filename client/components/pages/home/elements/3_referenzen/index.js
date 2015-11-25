44937276052f2b806851db002e2c6c92c0d75749
eljefedelrodeodeljefe <robert.lindstaedt@gmail.com>
2015-11-25 23:23:48 +0100

    Removes: bifi from leistunge -> to ref

--- client/components/pages/home/elements/3_referenzen/index.js
+++ client/components/pages/home/elements/3_referenzen/index.js
@@ -5,43 +5,57 @@ module.exports = {
       links: [
         {
           name: 'Chakuza & Curse',
+          type: 'image',
           external_link: '#',
-          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447630443/Chakuza-Curese-FB-Event-A_ibo6wr.jpg'
+          link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447630443/Chakuza-Curese-FB-Event-A_ibo6wr.jpg'
         },
         {
           name: '',
+          type: 'image',
           external_link: '#',
-          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447630443/Traumfrauen-WP2_v4xxol.jpg',
+          link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447630443/Traumfrauen-WP2_v4xxol.jpg',
         },
         {
           name: 'THE BLUEPRINT - FÉTE DE L´ORIGINALS',
+          type: 'image',
           external_link: 'https://www.facebook.com/media/set/?set=a.331138503715334.1073741835.293954250767093&type=3',
-          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447630443/10426765_4292954777773_7009074514962987474_n_brbmpt.jpg'
+          link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447630443/10426765_4292954777773_7009074514962987474_n_brbmpt.jpg'
         },
         {
           name: '',
+          type: 'image',
           external_link: '#',
-          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447630442/10404165_774527639285100_6229034639274008671_n_x48tp4.jpg'
+          link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447630442/10404165_774527639285100_6229034639274008671_n_x48tp4.jpg'
         },
         {
           name: 'ADBOYS4LIFE - MR.BOBFILMS X DOJO/FUCKINGYEAH X EPIC!DEVELOPMENT',
+          type: 'image',
           external_link: 'https://www.facebook.com/media/set/?set=a.306535136175671.1073741832.293954250767093&type=3',
-          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447630442/10259951_10151979979676783_635858087890241140_n_icfloa.jpg'
+          link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447630442/10259951_10151979979676783_635858087890241140_n_icfloa.jpg'
         },
         {
           name: '',
+          type: 'image',
           external_link: '#',
-          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447630442/995624_423835524445631_5365721727983521164_n_pawkqb.jpg'
+          link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447630442/995624_423835524445631_5365721727983521164_n_pawkqb.jpg'
         },
         {
           name: '',
+          type: 'image',
           external_link: '#',
-          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447630442/156057_345829545579563_5967578341148828480_n_z9e71u.jpg'
+          link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447630442/156057_345829545579563_5967578341148828480_n_z9e71u.jpg'
         },
         {
           name: '',
+          type: 'image',
           external_link: '#',
-          image_link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447630442/1653459_374818349365867_3036915616163435448_n_fpwyl4.jpg'
+          link: 'http://res.cloudinary.com/jefe-io/image/upload/v1447630442/1653459_374818349365867_3036915616163435448_n_fpwyl4.jpg'
+        },
+        {
+          name: '',
+          type: 'video',
+          external_link: '#',
+          link: 'https://www.youtube.com/embed/y1PhoiKOIvQ'
         }
       ]
     }
