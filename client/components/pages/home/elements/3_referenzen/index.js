bea94a88d2ae12f7facaddaee4ccb60dffbf863f
eljefedelrodeodeljefe <robert.lindstaedt@gmail.com>
2015-11-26 22:47:49 +0100

    Amend

--- client/components/pages/home/elements/3_referenzen/index.js
+++ client/components/pages/home/elements/3_referenzen/index.js
@@ -71,7 +71,7 @@ module.exports = {
           type: 'image',
           external_link: 'https://www.youtube.com/embed/y1PhoiKOIvQ',
           link: 'http://res.cloudinary.com/jefe-io/image/upload/v1448567245/bifi_ugtyog.jpg',
-          caption: 'Werbefilmkomposition „BiFi SnackPack“ TV und Online'
+          caption: 'Werbemusikkomposition „BiFi SnackPack“ TV und Online'
         }
       ]
     }
