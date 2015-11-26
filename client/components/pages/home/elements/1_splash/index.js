7ee1249fa7d4cc4bc6b35aeff910efe6c71adb24
eljefedelrodeodeljefe <robert.lindstaedt@gmail.com>
2015-11-26 19:48:52 +0100

    Change Leistungen to combined points

--- client/components/pages/home/elements/1_splash/index.js
+++ client/components/pages/home/elements/1_splash/index.js
@@ -4,7 +4,6 @@ let links = [
   // 'http://res.cloudinary.com/jefe-io/image/upload/c_scale,w_1314/v1447617124/1548168_331138637048654_2974130967997751976_o_abegam.jpg',
   'http://res.cloudinary.com/jefe-io/image/upload/c_scale,w_1322/v1447617129/10273303_295558303940021_3122393801162874264_o-1_kbap6k.jpg',
   'http://res.cloudinary.com/jefe-io/image/upload/c_scale,w_1322/v1447617127/1911020_331141580381693_523657765020535647_o_tc7yxu.jpg',
-  'http://res.cloudinary.com/jefe-io/image/upload/v1447757102/splash-home_zhsrro_pdh7ue.jpg'
 ]
 module.exports = {
   template: require('./template.html'),
